a550f8c00a4d937e28d32b1e7abdc35d4a7a2274
Cesar Escalona <cesarescalona10@gmail.com>
2021-05-18 12:35:56 -0400

    Created a class for my pet

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+// This is my controller for the Dating project
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require autoload file
+require_once ('vendor/autoload.php');
+
+// Require the pet class
+require('pet.php');
+
+// Start a session
+// session_start();
+
+// Instantiate Fat-Fre
+$f3 = Base::instance();
+
+// Define default route
+$f3->route('GET /', function(){
+    echo "<h1>Hello Pets!</h1>";
+
+    // Instantiate a pet onj
+    $pet1 = new Pet();
+
+    // Invoke the eat method
+    $pet1->sleep();
+});
+
+// Run Fat-Free
+$f3->run();
